aace850beb58d6121d3f317640cf9a54fc7cd70d
Jonas A. Hultén <jonas@sldr.se>
2022-02-09 23:12:28 +0100

    Bashed out a very first test
    Baby's first hash function. Also, unclear if I'll be using rayon myself,
    but I'm glad jwalk is using it.

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,17 @@
+//use sha3::{Digest, Sha3_256};
+
 #[cfg(test)]
 mod tests {
+    use hex_literal::hex;
+    use sha3::{Digest, Sha3_256};
     #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    fn basic_sha256() {
+        let mut hasher = Sha3_256::new();
+        hasher.update(b"coffee");
+        let result = hasher.finalize();
+        assert_eq!(
+            result[..],
+            hex!("2250fa0b557f93b1d92a26e2ca55cfe2354e416e9d674277784cfb09184b41b0")[..]
+        );
     }
 }
