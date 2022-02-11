ac743e807b927b7665ce2c266c470a4d88c56a8c
Jonas A. Hultén <jonas@sldr.se>
2022-02-12 00:03:49 +0100

    Added skeleton and test for hasher

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,32 @@
+use jwalk::WalkDir;
 use sha3::{Digest, Sha3_256};
+use std::fs;
+
+#[derive(Debug, Clone, Copy)]
+enum NodeType {
+    Directory,
+    File,
+    Symlink,
+}
+
+impl NodeType {
+    pub fn to_u8(&self) -> u8 {
+        match self {
+            NodeType::Directory => 2,
+            NodeType::File => 3,
+            NodeType::Symlink => 5,
+        }
+    }
+}
+
+pub fn hash_directory(path: &str) -> &[u8] {
+    &[]
+}
 
 #[cfg(test)]
 mod tests {
-    use hex_literal::hex;
     use crate::{Digest, Sha3_256};
+    use hex_literal::hex;
     #[test]
     fn basic_sha256() {
         let mut hasher = Sha3_256::new();
@@ -13,5 +36,14 @@ mod tests {
             result[..],
             hex!("2250fa0b557f93b1d92a26e2ca55cfe2354e416e9d674277784cfb09184b41b0")[..]
         );
+    }
+
+    #[test]
+    fn hash_test_data_one() {
+        let result = crate::hash_directory("test_data/one");
+        assert_eq!(
+            result[..],
+            hex!("40f97a3ba6ffd2d2eb95a65eb5c1de7ecf92119d0c9813362320e6a999a60c09")
+        )
     }
 }
