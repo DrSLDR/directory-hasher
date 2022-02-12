b307c7b139fe133c297c9ab75451ebfa8bd5c01f
Jonas A. Hultén <jonas@sldr.se>
2022-02-13 00:10:20 +0100

    Added some helpful documentation
    For some value of "helpful"

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,29 @@ use jwalk::WalkDir;
 use sha3::{Digest, Sha3_256};
 use std::fs;
 
+
+/// Defines the different types of nodes the directory walk can encounter. These types
+/// define, through the `to_u8` implementation, the byte that will be inserted between a
+/// node's name and content hash.
 #[derive(Debug, Clone, Copy)]
 enum NodeType {
+    /// A Directory node is a directory - this byte will be inserted between the
+    /// directory's name hash and its first content hash.
     Directory,
+    /// The Directory Separator is used between content hashes for a directory. While a
+    /// normal file will have exactly one name and content hash, a directory can have an
+    /// arbitrary number of content hashes - one per node in the directory. The
+    /// Directory Separator byte is inserted between these hashes.
     DirSeparator,
+    /// A File is any normal file - this byte will be inserted between the file's name
+    /// hash and its content hash.
     File,
+    /// A Symlink is a symbolic link (on platforms that support it; specifically, where
+    /// [`is_symlink`] returns true). This byte is appended to the end of a symbolic
+    /// link's name hash; since the hasher won't follow the link, the node won't have a
+    /// content hash.
+    ///
+    /// [`is_symlink`]: std::path::Path::is_symlink
     Symlink,
 }
 
