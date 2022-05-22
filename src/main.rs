eb8beb977eb517b0f8e3de93019b92d50d814046
Jonas A. Hultén <jonas@sldr.se>
2022-05-22 23:29:00 +0200

    Tidied up the main function
    It was useful for development, less so now that I have something that
    behaves

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,4 @@
-use sha3::{Digest, Sha3_256};
-use std::fmt::Write;
-use std::fs;
-use walkdir::WalkDir;
-
 fn main() -> Result<(), std::fmt::Error> {
-    for entry in WalkDir::new("test_data").sort_by_file_name().contents_first(true) {
-        let entry = match entry {
-            Ok(v) => v,
-            Err(_) => continue,
-        };
-        println!("{}", entry.path().display());
-    }
+    println!("Hello world, there are no hashes here");
     Ok(())
 }
